6433c71ecf690310a461a9349f302d80613cdc30
Tshifhiwa Madima <desmond_madima@yahoo.com>
2019-10-12 14:44:39 +0200

    py

--- mapping/mapfile.py
+++ mapping/mapfile.py
@@ -22,24 +22,31 @@ map = folium.Map(location=[38.58,-99.09], zoom_start=5, tiles="Mapbox Bright")
 
 #map = folium.Map(location=[0, 0], zoom_start=6)
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 #for lt,ln,el in zip(lat,lon,elev):
  #    fg.add_child(folium.Marker(location=[lt, ln],popup=str(el)+" m",icon=folium.Icon(color=color_producer(el)))
 	 
 for lt,ln,el in zip(lat,lon,elev):
-     fg.add_child(folium.CircleMarker(location=[lt, ln],popup=str(el)+" m",
+     fgv.add_child(folium.CircleMarker(location=[lt, ln],popup=str(el)+" m",
 	 fill_color=color_producer(el),color = 'grey',fill_opacity=0.7))
 	 #icon=folium.Icon(color=color_producer(el))))	 
      # 	 
 
+fgp = folium.FeatureGroup(name="Population")	 
+	 
 with open('world.json','r',encoding='utf-8-sig') as json_file:
     worldJSON = json.load(json_file)
 
 
-fg.add_child(folium.GeoJson(worldJSON,
+fgp.add_child(folium.GeoJson(worldJSON,
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 
 else 'orange' if 1000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+
+
 	 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("map092.html")	
