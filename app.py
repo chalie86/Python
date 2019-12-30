c4def15f32fc53a116ffe47adb6905980e71a608
python <tdmadima@fmail.com>
2019-12-30 20:30:16 +0200

    db commit

---
+++ app.py
@@ -0,0 +1,6 @@
+from user import User
+
+
+my_user = User('SAM','SAM@gmail.com','JOE',None)
+
+my_user.save_to_db()
---
+++ user.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+
+class User:
+    def __init__(self, first_name, email, last_name, id):
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.id = id
+
+    def __repr__(self):
+        return "<User {}>".format(self.email)
+
+    def save_to_db(self):
+        with psycopg2.connect(dbname="learning", user="postgres", password="postgres", host="localhost",
+                              port="5432") as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('INSERT INTO USERS(email,first_name,last_name) VALUES(%s,%s,%s)',
+                               (self.email, self.first_name, self.last_name))
+
+    @classmethod
+    def load_from_db_by_email(cls, email):
+        with psycopg2.connect(dbname="learning", user="postgres", password="postgres", host="localhost",
+                              port="5432") as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('select * from users where email=%s', (email,))
+                user_data = cursor.fetchone()
+                return cls(user_data[1],user_data[2],user_data[3],user_data[0])
